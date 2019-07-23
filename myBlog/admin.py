c4c16769a5afee124aa80e7c4800f9e3446ad862
Loner <yinjiakang@gmail.com>
2019-07-23 10:27:17 +0800

    desktop

--- myBlog/admin.py
+++ myBlog/admin.py
@@ -4,16 +4,6 @@ from myBlog.models import *
 
 # Register your models here.
 
-
-# class ArticleAdmin(admin.ModelAdmin):
-#     class Media:
-#         js = (
-#             '/static/js/kindeditor-4.1.10/kindeditor-min.js',
-#             '/static/js/kindeditor-4.1.10/lang/zh_CN.js',
-#             '/static/js/kindeditor-4.1.10/config.js',
-#         )
-
-
 admin.site.register(User)
 admin.site.register(Tag)
 admin.site.register(Article)
--- myBlog/static/js/kindeditor-4.1.10/config.js
+++
@@ -1,7 +0,0 @@
-KindEditor.ready(function(K) {
-                K.create('textarea[name=content]',{
-                    width:'800px',
-                    height:'200px',
-                    uploadJson: '/admin/upload/kindeditor',
-                });
-        });
